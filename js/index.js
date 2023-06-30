73828ea36a9f60c3fa78ae93d7651a28aba30f7d
Abdirahman abass diis <abdirahmanfarmajo7786@gmail.com>
2023-06-30 20:36:02 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -1,32 +1,76 @@
-var xmlHttp = null;
+document.addEventListener('DOMContentLoaded', () =>{
+    const form = document.querySelector("#github-form");
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        let search = e.target.search.value
+        //console.log(search)
+        handleSearch(search)
+    
+function handleSearch() {
+    
+    fetch('https://api.github.com/search/users?q=' + search, {
+        method: 'GET',
+        header:{
+            'Content-Type': 'application/json',
+            Accept: 'application/vnd.github.v3+json'
+        },
+        body: JSON.stringify()
+    })
+    .then(res => res.json())
+    .then(data => {console.log(data)
+        document.querySelector('#user-list').innerHTML = ''
+        document.querySelector('#repos-list').innerHTML =''
+  
+        data.items.forEach(user => {
+            console.log(user)
+            let userCard = document.createElement('li')
+            userCard.className = 'all-users'
+            userCard.innerHTML = `
+                <div class='content'>
+                    <h3> User: ${user.login}</h3>
+                    <p> URL: ${user.html_url}</p>
+                    <div class ='repos'>
+                    <button class='repo-button' style='margin-bottom: 25px'>
+                    Repositories
+                    </button>
+                    </div>
+                    <img src=${user.avatar_url} />
+                </div>`
+            
+           document.querySelector('#user-list').appendChild(userCard)   
+           
+           const repoButton = document.querySelector('.repo-button')
+           console.log(repoButton)
+           repoButton.addEventListener('click', () => {
+               fetch(user.repos_url, {
+               method: 'GET',
+               header:{
+                   'Content-Type': 'application/json',
+                   Accept: 'application/vnd.github.v3+json'
+               },
+               body: JSON.stringify()
+            })
+               .then(res => res.json())
+               .then(data => {
+             
+               data.forEach(repo => {
+                  
+                    let repoCard = document.createElement('li')
+                    repoCard.innerHTML = `
+                    <h4> ${repo.name} </h4>
+                    <p> ${repo.html_url}</p>
+                    `
+                    document.querySelector('#repos-list').appendChild(repoCard)
+                    
+               })
+            })
+              
+           })
 
-function GetCustomerInfo()
-{
-    var CustomerNumber = document.getElementById( "TextBoxAccountName" ).value;
-    var Url = "GetAccountInfoAsJson.aspx?number=" + AccountName;
 
-    xmlHttp = new XMLHttpRequest(); 
-    xmlHttp.onreadystatechange = ProcessRequest;
-    xmlHttp.open( "GET", Url, true );
-    xmlHttp.send( null );
+    })
+
+})
 }
-
-function ProcessRequest() 
-{
-    if ( xmlHttp.readyState == 4 && xmlHttp.status == 200 ) 
-    {
-        if ( xmlHttp.responseText == "Not found" ) 
-        {
-            document.getElementById( "TextBoxAccountName"    ).value = "Not found";
-            document.getElementById( "TextBoxAccountAddress" ).value = "";
-        }
-        else
-        {
-            var info = eval ( "(" + xmlHttp.responseText + ")" );
-
-            // No parsing necessary with JSON!        
-            document.getElementById( "TextBoxAccountName"    ).value = info.jsonData[ 0 ].cmname;
-            document.getElementById( "TextBoxAccountAddress" ).value = info.jsonData[ 0 ].cmaddr1;
-        }                    
-    }
-}    
+})
+})  
